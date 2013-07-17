b49c9ff2746ea60241d99e78ae7d7d4863c8329a
pein0119 <pein0119@gmail.com>
2013-07-18 01:37:10 +0800

    add init.el

---
+++ init.el
@@ -0,0 +1,8 @@
+(add-to-list 'load-path "~/.emacs.d/config")
+
+(load "init-base.el")
+(load "init-face.el")
+(load "init-func.el")
+(load "init-dev.el")
+(load "init-elpa.el")
+(load "init-kbd.el")
