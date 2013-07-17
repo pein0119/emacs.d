1d0c3e5351de85467b800e62bf419373c919a0ef
pein0119 <pein0119@gmail.com>
2013-07-18 01:24:43 +0800

    rm config&lib

--- lib/pyflakes/flymake-python-pyflakes-autoloads.el
+++
@@ -1,29 +0,0 @@
-;;; flymake-python-pyflakes-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (flymake-python-pyflakes-load) "flymake-python-pyflakes"
-;;;;;;  "flymake-python-pyflakes.el" (20917 32276))
-;;; Generated autoloads from flymake-python-pyflakes.el
-
-(autoload 'flymake-python-pyflakes-load "flymake-python-pyflakes" "\
-Configure flymake mode to check the current buffer's python syntax using pyflakes.
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("flymake-python-pyflakes-pkg.el") (20917
-;;;;;;  32276 735890))
-
-;;;***
-
-(provide 'flymake-python-pyflakes-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; flymake-python-pyflakes-autoloads.el ends here
--- lib/pylint/pylint-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "pylint" "20121112.1335" "minor mode for running `pylint' [hg]" (quote nil))
