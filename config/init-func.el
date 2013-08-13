1a07b297d7c2348cbcd0dee9c1740441e6f5e790
pein0119 <pein0119@gmail.com>
2013-08-13 14:26:40 +0800

    add function to cp or kill current line int init-func.el

--- config/init-func.el
+++ config/init-func.el
@@ -93,3 +93,28 @@
     "#define " str "\n\n"
     _ "\n\n#endif"))
 
+;; 复制和剪切当前行
+;; copy region or whole line
+(global-set-key "\M-w"
+(lambda ()
+  (interactive)
+  (if mark-active
+      (kill-ring-save (region-beginning)
+      (region-end))
+    (progn
+     (kill-ring-save (line-beginning-position)
+     (line-end-position))
+     (message "copied line")))))
+
+
+;; kill region or whole line
+(global-set-key "\C-w"
+(lambda ()
+  (interactive)
+  (if mark-active
+      (kill-region (region-beginning)
+   (region-end))
+    (progn
+     (kill-region (line-beginning-position)
+  (line-end-position))
+     (message "killed line")))))
