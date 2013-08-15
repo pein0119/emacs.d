39410da2db377abe2a7dfb168c27ea6a1520ffb0
pein0119 <pein0119@gmail.com>
2013-08-15 15:59:27 +0800

    erase lib smart-operator

--- elpa/smart-operator-20051013.1756/smart-operator-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; smart-operator-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("smart-operator-pkg.el" "smart-operator.el")
-;;;;;;  (20986 17060 494559))
-
-;;;***
-
-(provide 'smart-operator-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; smart-operator-autoloads.el ends here
--- elpa/smart-operator-20051013.1756/smart-operator-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "smart-operator" "20051013.1756" "Beautify the operators in codes [wiki]" (quote nil))
