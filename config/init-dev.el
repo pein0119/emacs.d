bfa7ac5e2c1d2d6fccf5cc38adee6be43a9b8da5
pein0119 <pein0119@gmail.com>
2013-11-16 11:16:04 +0800

    disable helm-gtags

--- config/init-dev.el
+++ config/init-dev.el
@@ -74,11 +74,3 @@
 ;; 按装helm-gtags
 (require 'helm-config)
 (require 'helm-gtags)
-
-(add-hook 'helm-gtags-mode-hook
-		  '(lambda ()
-			 (local-set-key (kbd "M-t") 'helm-gtags-find-tag)
-			 (local-set-key (kbd "M-r") 'helm-gtags-find-rtag)
-			 (local-set-key (kbd "M-s") 'helm-gtags-find-symbol)
-			 (local-set-key (kbd "C-t") 'helm-gtags-pop-stack)
-			 (local-set-key (kbd "C-c C-f") 'helm-gtags-find-files)))
--- lib/helm-gtags/helm-gtags-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "helm-gtags" "20131112.1206" "GNU GLOBAL helm interface [github]" (quote ((helm "1.0"))))
