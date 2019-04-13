01fdec3a3bcb33a0bc1f15b5b4ec28d60485ab87
zhangxiaocong <zhangxiaocong@bytedance.com>
2019-04-14 00:21:00 +0800

    fix ido warning

--- config/ido-settings.el
+++ config/ido-settings.el
@@ -1,5 +1,12 @@
 ;;; ido的配置
 
+;; Fix for the warnings
+(defvar ido-cur-item nil)
+(defvar ido-default-item nil)
+(defvar ido-cur-list nil)
+(defvar predicate nil)
+(defvar inherit-input-method nil)
+
 (setq ido-enable-flex-matching t    
 	  ido-create-new-buffer 'always 
 	  ido-use-filename-at-point 'guess 
